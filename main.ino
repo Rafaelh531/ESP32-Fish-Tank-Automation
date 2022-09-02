382a9148abd4f95547eb1eb0a1e3a4cb2274cdb7
Rafael da Rosa <101129583+Rafaelh531@users.noreply.github.com>
2022-09-02 11:51:41 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,5 +1,3 @@
-
-// Import required libraries
 #ifdef ESP32
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
@@ -12,9 +10,47 @@
 #endif
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SSD1306.h>
 
+// Pinos do display (comunicação i2c)
+const int DISPLAY_ADDRESS_PIN = 0x3c;
+const int DISPLAY_SDA_PIN = 4;
+const int DISPLAY_SCL_PIN = 15;
+const int DISPLAY_RST_PIN = 16;
+
+// Altura da fonte (correspondente a fonte ArialMT_Plain_16)
+const int fontHeight = 8   ;
+
+// Objeto do display
+SSD1306 display(DISPLAY_ADDRESS_PIN, DISPLAY_SDA_PIN, DISPLAY_SCL_PIN);
 // Data wire is connected to GPIO 4
 #define ONE_WIRE_BUS 17
+#define LED 12
+#define RESISTENCIA 13
+
+float ATUAL;
+float DESEJADA = 30;
+bool displayBegin()
+{
+  // Reiniciamos o display
+  pinMode(DISPLAY_RST_PIN, OUTPUT);
+  digitalWrite(DISPLAY_RST_PIN, LOW);
+  delay(1);
+  digitalWrite(DISPLAY_RST_PIN, HIGH);
+  delay(1);
+
+  return display.init();
+}
+void displayConfig()
+{
+  // Invertemos o display verticalmente
+  display.flipScreenVertically();
+  // Setamos a fonte
+  display.setFont(ArialMT_Plain_10);
+  // Alinhamos a fonta à esquerda
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+}
+
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
@@ -30,8 +66,8 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 1000;
 
 // Replace with your network credentials
-const char* ssid = "Rafael";
-const char* password = "confidencial";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -47,6 +83,35 @@ String readDSTemperatureC() {
   } else {
     Serial.print("Temperature Celsius: ");
     Serial.println(tempC);
+    ATUAL = tempC;
+    // Limpamos o display
+    display.clear();
+      display.setFont(ArialMT_Plain_10);
+    // Escrevemos a mensagem "Sending packet: " na primeira linha
+    display.drawString(0, 0, "TEMPERATURA ATUAL: ");
+      display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 10,String(ATUAL) + "°C");
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 26, "TEMPERATURA DESEJADA: ");
+     display.setFont(ArialMT_Plain_16);
+     display.drawString(0, 36,String(DESEJADA) + "°C");
+    // Exibimos as alterações no display
+    display.display();
+
+    
+    if (digitalRead(LED) == LOW) {
+      if (ATUAL < (DESEJADA - 0.5)) {
+        digitalWrite(LED, HIGH);
+        digitalWrite(RESISTENCIA, LOW);
+      }
+
+    }
+    else if (digitalRead(LED) == HIGH) {
+      if (ATUAL > DESEJADA) {
+        digitalWrite(LED, LOW);
+        digitalWrite(RESISTENCIA, HIGH);
+      }
+    }
   }
   return String(tempC);
 }
@@ -77,15 +142,18 @@ const char index_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
   <h2>  <i class="fa-solid fa-fish-fins" style="color:#059e8a;"></i>  AQUÁRIO  <i class="fa-solid fa-fish-fins" style="color:#059e8a;"></i>  </h2>
- 
+
   <p>
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
     <span class="ds-labels">Temperatura Atual:</span> 
     <span id="temperaturec">%TEMPERATUREC%</span>
     <sup class="units">&deg;C</sup>
   </p>
-    <span class="ds-labels" id="valBox"></span>
-    <input type="range" min="20.0" max="40.0" value="20.0" step="0.25" onchange="showVal(this.value)">
+    <p>
+     <span class="ds-labels">Temperatura Desejada:</span> 
+    <span class="ds-labels" id="valBox"> 30.0</span>
+      </p>
+    <input type="range" min="20.0" max="40.0" value="30.0" step="0.25" onchange="showVal(this.value)">
 </body>
 <script language = "JavaScript">
 function showVal(newVal){
@@ -103,7 +171,7 @@ setInterval(function ( ) {
   };
   xhttp.open("GET", "/temperaturec", true);
   xhttp.send();
-}, 2000) ;
+}, 1000) ;
 
 </script>
 </html>)rawliteral";
@@ -121,7 +189,22 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
   Serial.println();
+  pinMode(LED, OUTPUT);
+  pinMode(RESISTENCIA, OUTPUT);
+  digitalWrite(LED, LOW);
+  digitalWrite(RESISTENCIA, LOW);
 
+  // Iniciamos o display
+  if (!displayBegin())
+  {
+    // Se não deu certo, exibimos falha de display na serial
+    Serial.println("Display failed!");
+    // E deixamos em loop infinito
+    while (1);
+  }
+
+  // Configuramos o posicionamento da tela, fonte e o alinhamento do texto
+  displayConfig();
   // Start up the DS18B20 library
   sensors.begin();
 
@@ -149,13 +232,26 @@ void setup() {
   server.on("/post", HTTP_POST, [](AsyncWebServerRequest * request) {},
   NULL,
   [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
-
+    String tempor = "";
     for (size_t i = 0; i < len; i++) {
-      Serial.write(data[i]);
+      tempor += char(data[i]);
+      //Serial.write(data[i]);
     }
-
-    Serial.println();
-
+    DESEJADA = tempor.toFloat();
+    Serial.println(DESEJADA);
+        display.clear();
+      display.setFont(ArialMT_Plain_10);
+    // Escrevemos a mensagem "Sending packet: " na primeira linha
+    display.drawString(0, 0, "TEMPERATURA ATUAL: ");
+      display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 10,String(ATUAL) + "°C");
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 26, "TEMPERATURA DESEJADA: ");
+     display.setFont(ArialMT_Plain_16);
+     display.drawString(0, 36,String(DESEJADA) + "°C");
+    // Exibimos as alterações no display
+    display.display();
+    //Serial.println("aqui " + String(DESEJADA));
     request->send(200);
   });
 
